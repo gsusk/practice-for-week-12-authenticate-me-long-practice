34fba3e2c4e057083356b1d2eb918e19e5ab437b
gsus <satehortuamercado@gmail.com>
2023-11-23 16:22:36 -0500

    set initial middlewares

---
+++ backend/app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+require('express-async-errors')
+const morgan = require('morgan')
+const cors = require('cors')
+const csurf = require('csurf')
+const helmet = require('helmet')
+const cookieParser = require('cookie-parser')
+const { environment } = require('./config')
+
+const isProduction = environment === 'production'
+
+const app = express()
+
+if (!isProduction) {
+  app.use(cors())
+}
+app.use(morgan('dev'))
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(cookieParser())
+app.use(express.json())
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && 'Lax',
+      httpOnly: true,
+    },
+  }),
+)
