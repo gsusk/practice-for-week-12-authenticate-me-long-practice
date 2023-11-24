b11ee8d9c1123aca7321a6418c05c8b3c7ba20b3
gsus <satehortuamercado@gmail.com>
2023-11-24 14:17:01 -0500

    added user model

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,12 @@
 const router = require('express').Router()
+const apiRouter = require('./api')
 
-router.get('/hello/world', (req, res, next) => {
-  res.cookie('XSRF-TOKEN', req.csrfToken())
-  res.send('Hello World!')
+router.use('/api', apiRouter)
+
+router.get('/api/csrf/restore', (req, res) => {
+  const csrfToken = req.csrfToken()
+  res.cookie('XSRF-TOKEN', csrfToken)
+  res.status(200).json({ 'XSRF-TOKEN': csrfToken })
 })
 
 module.exports = router
